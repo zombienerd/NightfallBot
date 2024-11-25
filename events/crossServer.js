21704de471002393e806854098ef26449eb8ee7c
zombienerd <ccr@ccrvb.com>
2024-11-25 12:18:56 -0500

    modified:   events/crossServer.js

--- events/crossServer.js
+++ events/crossServer.js
@@ -35,7 +35,7 @@ const {
     channelWHID9, 
     channelWHTK9, 
 //    channelWHID10, 
- //   channelWHTK10 
+//    channelWHTK10 
 } = require('../config.json');
 
 const serverAcronyms = {
@@ -64,12 +64,10 @@ module.exports = {
            channel2MessageId: null,
            channel3MessageId: null,
            channel4MessageId: null,
-           // channel5MessageId: null,
            channel6MessageId: null,
            channel7MessageId: null,
            channel8MessageId: null,
            channel9MessageId: null,
-           //channel10MessageId: null
         };
 
         // Set the correct field in mappings based on the origin server
@@ -78,71 +76,57 @@ module.exports = {
             targetWebhooks.push({ id: channelWHID2, token: channelWHTK2 });
             targetWebhooks.push({ id: channelWHID3, token: channelWHTK3 });
             targetWebhooks.push({ id: channelWHID4, token: channelWHTK4 });
-            //targetWebhooks.push({ id: channelWHID5, token: channelWHTK5 });
         } else if (message.channel.id === channelID2) {
             mappings.channel2MessageId = message.id;
             targetWebhooks.push({ id: channelWHID1, token: channelWHTK1 });
             targetWebhooks.push({ id: channelWHID3, token: channelWHTK3 });
             targetWebhooks.push({ id: channelWHID4, token: channelWHTK4 });
-            //targetWebhooks.push({ id: channelWHID5, token: channelWHTK5 });
         } else if (message.channel.id === channelID3) {
             mappings.channel3MessageId = message.id;
             targetWebhooks.push({ id: channelWHID1, token: channelWHTK1 });
             targetWebhooks.push({ id: channelWHID2, token: channelWHTK2 });
             targetWebhooks.push({ id: channelWHID4, token: channelWHTK4 });
-            //targetWebhooks.push({ id: channelWHID5, token: channelWHTK5 });
         } else if (message.channel.id === channelID4) {
             mappings.channel4MessageId = message.id;
             targetWebhooks.push({ id: channelWHID1, token: channelWHTK1 });
             targetWebhooks.push({ id: channelWHID2, token: channelWHTK2 });
             targetWebhooks.push({ id: channelWHID3, token: channelWHTK3 });
-            //targetWebhooks.push({ id: channelWHID5, token: channelWHTK5 });
-        //} else if (message.channel.id === channelID5) {
-            //mappings.channel5MessageId = message.id;
-            //targetWebhooks.push({ id: channelWHID1, token: channelWHTK1 });
-            //targetWebhooks.push({ id: channelWHID2, token: channelWHTK2 });
-            //targetWebhooks.push({ id: channelWHID3, token: channelWHTK3 });
-            //targetWebhooks.push({ id: channelWHID4, token: channelWHTK4 });
         } else if (message.channel.id === channelID6) {
             mappings.channel6MessageId = message.id;
             targetWebhooks.push({ id: channelWHID7, token: channelWHTK7 });
             targetWebhooks.push({ id: channelWHID8, token: channelWHTK8 });
             targetWebhooks.push({ id: channelWHID9, token: channelWHTK9 });
-            //targetWebhooks.push({ id: channelWHID10, token: channelWHTK10 });
         } else if (message.channel.id === channelID7) {
             mappings.channel7MessageId = message.id;
             targetWebhooks.push({ id: channelWHID6, token: channelWHTK6 });
             targetWebhooks.push({ id: channelWHID8, token: channelWHTK8 });
             targetWebhooks.push({ id: channelWHID9, token: channelWHTK9 });
-            //targetWebhooks.push({ id: channelWHID10, token: channelWHTK10 });
         } else if (message.channel.id === channelID8) {
             mappings.channel8MessageId = message.id;
             targetWebhooks.push({ id: channelWHID6, token: channelWHTK6 });
             targetWebhooks.push({ id: channelWHID7, token: channelWHTK7 });
             targetWebhooks.push({ id: channelWHID9, token: channelWHTK9 });
-            //targetWebhooks.push({ id: channelWHID10, token: channelWHTK10 });
         } else if (message.channel.id === channelID9) {
             mappings.channel9MessageId = message.id;
             targetWebhooks.push({ id: channelWHID6, token: channelWHTK6 });
             targetWebhooks.push({ id: channelWHID7, token: channelWHTK7 });
             targetWebhooks.push({ id: channelWHID8, token: channelWHTK8 });
-            //targetWebhooks.push({ id: channelWHID10, token: channelWHTK10 });
-        //} else if (message.channel.id === channelID10) {
-            //mappings.channel10MessageId = message.id;
-            //targetWebhooks.push({ id: channelWHID6, token: channelWHTK6 });
-            //targetWebhooks.push({ id: channelWHID7, token: channelWHTK7 });
-            //targetWebhooks.push({ id: channelWHID8, token: channelWHTK8 });
-            //targetWebhooks.push({ id: channelWHID9, token: channelWHTK9 });         
         }
 
         for (const webhookData of targetWebhooks) {
             const webhookClient = new WebhookClient(webhookData);
 
+            // Include stickers if available
+            const stickerURL = message.stickers.size > 0 ? message.stickers.first().url : null;
+
             const messageOptions = {
                 username: webhookUsername,
                 avatarURL: avatarURL,
-                content: message.content || undefined,
-                files: message.attachments.map(att => att.url),
+                content: message.content || (stickerURL ? `[Sticker](${stickerURL})` : undefined),
+                files: [
+                    ...message.attachments.map(att => att.url),
+                    ...(stickerURL ? [stickerURL] : [])
+                ],
                 embeds: message.embeds.length > 0 ? message.embeds.map(embed => ({
                     title: embed.title,
                     description: embed.description,
@@ -156,6 +140,12 @@ module.exports = {
                 })) : undefined
             };
 
+            // Ensure the message isn't empty
+            if (!messageOptions.content && messageOptions.files.length === 0 && !messageOptions.embeds) {
+                console.error('Skipping message: Cannot send an empty message');
+                continue;
+            }
+
             const relayedMessage = await webhookClient.send(messageOptions);
 
             // Update the mapping with the relayed message IDs
@@ -163,17 +153,13 @@ module.exports = {
             if (webhookData.id === channelWHID2) mappings.channel2MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID3) mappings.channel3MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID4) mappings.channel4MessageId = relayedMessage.id;
-            //if (webhookData.id === channelWHID5) mappings.channel5MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID6) mappings.channel6MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID7) mappings.channel7MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID8) mappings.channel8MessageId = relayedMessage.id;
             if (webhookData.id === channelWHID9) mappings.channel9MessageId = relayedMessage.id;
-            //if (webhookData.id === channelWHID10) mappings.channel10MessageId = relayedMessage.id;
         }
 
         // Save original and relayed message IDs to the database
-        // console.log(`Adding mapping to database for message ID ${message.id}:`, mappings);
         await db.addMessageMapping(message.id, mappings);
-        // console.log(`Mapping added for source message ID ${message.id}:`, mappings);
     },
 };
